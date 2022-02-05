f61a4ef77f18b3a30d290e52ff37716c148bc828
jonathan35622 <73504770+jonathan35622@users.noreply.github.com>
2022-02-05 00:06:15 -0300

    Update Electrodomestico.java

--- src/models/Electrodomestico.java
+++ src/models/Electrodomestico.java
@@ -1,57 +1,44 @@
 package models;
-
 /**
  *
- * @author WIN-10
+ * @author Jsuarez
  */
 public class Electrodomestico {
-    //constructor con 4 parámetros
+    //constructor sobrecargado
     public Electrodomestico( double peso, double precioBase, char consumoEnergetico,String color){
         this.peso = peso;
         this.precioBase = precioBase; 
         this.consumoEnergetico = consumoEnergetico;
         this.color = color;
    }
-    
    //atributos privados
     private String color;
     private double peso;
     private char consumoEnergetico;
     private double precioBase;
-    
     //Métodos set y get atributos Electrodomestico
     public String getColor() {
         return color;
     }
-
     public void setColor(String color) {
         this.color = color;
     }
-
     public double getPeso() {
         return peso;
     }
-
     public void setPeso(double peso) {
         this.peso = peso;
     }
-
     public char getConsumoEnergetico() {
         return consumoEnergetico;
     }
-
     public void setConsumoEnergetico(char consumoEnergetico) {
         this.consumoEnergetico = consumoEnergetico;
     }
-
     public double getPrecioBase() {
         return precioBase;
     }
-
     public void setPrecioBase(double precioBase) {
         this.precioBase = precioBase;
     }
-    
-       
-
 }
